f9f1f7a4699a0c1141646e73ed7512331c12f090
andy <563915740@qq.com>
2017-01-18 17:17:59 +0800

    1

--- Public/Template/Home/Index/index.php
+++ Public/Template/Home/Index/index.php
@@ -1,171 +1,85 @@
-<!DOCTYPE html>
-<html lang="zh">
-<head>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-    <meta name="author" content="me@evanxu"/>
-    <meta name="format-detection" content="telephone=no">
-    <meta name="apple-mobile-web-app-capable" content="yes">
-    <meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" name="viewport">
-    <title>首页</title>
-    <link href="/Public/Static/Home/css/bootstrap.min.css" type="text/css" rel="stylesheet">
-    <link href="/Public/Static/Home/css/css.css" type="text/css" rel="stylesheet">
-    <script src="/Public/Static/Home/js/swiper.js"></script>
-    <script src="/Public/Static/Home/js/zepto.min.js"></script>
-    <style>
-
-
-
-    </style>
-</head>
-<body>
-<div class="banner">
-    <div class="swiper-container1">
-        <div class="swiper-wrapper list_an">
-            <?php foreach ($adverts as $k=>$v) {?>
-            <div class="swiper-slide list"><img src="<?php echo $v['image'];?>" height="496" width="100%"></div>
-            <?php }?>
-        </div>
-        <div class="swiper-pagination"></div>
-    </div>
-</div>
-<div class="container">
-    <div class="row">
-        <div class="col-lg-9 swiper-container" style="height: 560px; overflow: hidden;">
-            <div class="swiper-wrapper">
-                <?php if (!empty($demands)) {?>
-                    <?php foreach ($demands as $k=>$v) { ?>
-                        <div class="row cont_row swiper-slide">
-                            <span class="col-lg-2 img">
-                                <img src="/Public/Static/Home/images/type_<?php echo $v['typeid'];?>.png" height="100%">
-                            </span>
-                            <span class="col-lg-2"><?php echo $v['mrname'];?></span>
-                            <?php if ($v['typeid'] == 1) {?>
-                                <span class="col-lg-3">
-                                    投资项目：<?php echo $v['other']['name'];?>
-                                </span>
-                                <span class="col-lg-3">
-                                    投资金额：<?php echo $v['amount'];?>元
-                                </span>
-                            <?php } else if ($v['typeid'] == 2) {?>
-                                <span class="col-lg-3">
-                                    支持项目：<?php echo $v['other']['name'];?>
-                                </span>
-                                <span class="col-lg-3">
-                                    支持金额：<?php echo $v['amount'];?>元
-                                </span>
-                            <?php } else if ($v['typeid'] == 3) {?>
-                                <span class="col-lg-3">
-                                    借款金额：<?php echo $v['amount'];?>万元
-                                </span>
-                                <span class="col-lg-3">
-                                    借款期限：<?php echo $v['other']['term'];?>
-                                </span>
-                            <?php } else if ($v['typeid'] == 4) {?>
-                                <span class="col-lg-3">
-                                    典当金额：<?php echo $v['amount'];?>万元
-                                </span>
-                                <span class="col-lg-3">
-                                    典当期限：<?php echo $v['other']['term'];?>
-                                </span>
-                            <?php } else if ($v['typeid'] == 5) {?>
-                                <span class="col-lg-3">
-                                    融资金额：<?php echo $v['amount'];?>万元
-                                </span>
-                                <span class="col-lg-3">
-                                    租赁期限：<?php echo $v['other']['term'];?>
-                                </span>
-                            <?php } else if ($v['typeid'] == 6) {?>
-                                <span class="col-lg-3">公司名称：<?php echo $v['other']['company'];?>
-                                </span>
-                                <span class="col-lg-3">
-                                拟IPO市场：<?php echo $v['other']['ipo'];?></span>
-                            <?php } else if ($v['typeid'] == 7) {?>
-                                <span class="col-lg-3">公司名称：<?php echo $v['other']['company'];?>
-                                </span>
-                                <span class="col-lg-3">
-                                拟融资规模：<?php echo $v['amount'];?>万元</span>
-                            <?php } else if ($v['typeid'] == 8) {?>
-                                <span class="col-lg-3">
-                                    投资金额：<?php echo $v['amount'];?>万元
-                                </span>
-                                <span class="col-lg-3">
-                                    投资期限：<?php echo $v['other']['term'];?>
-                                </span>
-                            <?php } else if ($v['typeid'] == 9) {?>
-                                <span class="col-lg-3">
-                                    投资金额：<?php echo $v['amount'];?>万元
-                                </span>
-                                <span class="col-lg-3">
-                                    投资期限：<?php echo $v['other']['term'];?>
-                                </span>
-                            <?php } else if ($v['typeid'] == 10) {?>
-                                <span class="col-lg-3">
-                                    资管需求：<?php echo $v['other']['note'];?><br/>
-                                </span>                            
-                            <?php } else if ($v['typeid'] == 11) {?>
-                                <span class="col-lg-3">
-                                    产品名称：<?php echo $v['other']['name'];?>
-                                </span>
-                                <span class="col-lg-3">
-                                    支付金额：<?php echo $v['amount'];?>元<br/>
-                                </span>                            
-                            <?php } else if ($v['typeid'] == 12) {?>
-                                <span class="col-lg-3">
-                                    公司名称：<?php echo $v['other']['company'];?>
-                                </span>
-                                <span class="col-lg-3">
-                                    咨询类型：<?php echo $v['other']['zxtype'];?>
-                                </span>                            
-                            <?php }?>
-                            <span class="col-lg-2 row_dz">
-                                <?php if (!in_array($v['typeid'], array(1,2,11))) {?>
-                                    <img src="/Public/Static/Home/images/wz.png"><?php echo $v['city_name'];?>.<?php echo $v['area_name'];?>
-                                <?php }?>
-                            </span>
-                        </div>
-                    <?php }?>
-                <?php } else {?>
-                
-                <?php }?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <title>明星动态首页 - 明星资讯</title>
+        <link href="/Public/Static/Home/css/aiwen.css" rel="stylesheet" type="text/css" />
+    </head>
+    <body>
+        <div class="aw_header">            
+            <div class="aw_menus aw_menu_item">
+                <p>
+                    <a href="http://www.myhouse.com/news/" target="_blank" class="i">娱乐新闻</a>
+                    <a href="http://www.myhouse.com/news/mxdt/" title="明星动态"  target="_blank" id="c100">明星动态</a>
+                </p>
             </div>
         </div>
-        <div class="col-lg-3 wxcon">
-            <div class="col-lg-6 weix_con">
-                <span><img src="/Public/Static/Home/images/bt.jpg" width="148"> </span>
-                <p>八特金融</p>
-            </div>
-            <div class="col-lg-6 weix_con">
-                <span><img src="/Public/Static/Home/images/md.jpg" width="148"> </span>
-                <p>萌哒众筹</p>
-            </div>
-            <div class="col-lg-6 weix_con">
-                <span><img src="/Public/Static/Home/images/jhs.png" width="148"> </span>
-                <p>合金社</p>
-            </div>
-            <div class="col-lg-6 weix_con">
-                <span><img src="/Public/Static/Home/images/fqb.png" width="148"> </span>
-                <p>分期宝</p>
+        <div class="aw_body">
+            <div class="banner1100"></div>
+            <div class="aw_content">
+                <div class="left">
+                    <div class="weizhi"><b>当前位置：</b><a href="http://www.myhouse.com">明星网</a> &gt; <a href="http://www.myhouse.com/news/">星闻</a> &gt; <a href="http://www.myhouse.com/news/mxdt/">明星动态</a> &gt; <!--0.0034--> 首页</div>
+                    <div class="tit02"><h1>明星动态</h1></div>
+                    <div class="newslist">
+                        <dl>
+                            <dt>
+                                <a href="http://www.myhouse.com/news/102/305235001.html" target="_blank">
+                                    <img src="http://www.myhouse.com/upload/thumb/1018/305235.jpg" alt="郭德纲为21岁儿子郭麒麟现场征婚" title="郭德纲为21岁儿子郭麒麟现场征婚"/>
+                                </a>
+                            </dt>
+                            <dd class="t1">
+                                <span>2017-01-18 08:39:01</span>
+                                <a href="http://www.myhouse.com/news/102/305235001.html" target="_blank">郭德纲为21岁儿子郭麒麟现场征婚</a>
+                            </dd>
+                            <dd class="t2"> 1月16日，2016微博之夜在、黄子韬、张信哲、张…</dd>
+                        </dl>
+                        <dl>
+                            <dt>
+                                <a href="http://www.myhouse.com/news/102/305233001.html" target="_blank">
+                                    <img src="http://www.myhouse.com/upload/thumb/1018/305233.jpg" alt="曝傅园慧亮相央视春彩排现场" title="曝傅园慧亮相央视春彩排现场"/>
+                                </a>
+                            </dt>
+                            <dd class="t1">
+                                <span>2017-01-18 08:45:07</span>
+                                <a href="http://www.myhouse.com/news/102/305233001.html" target="_blank">曝傅园慧亮相央视春彩排现场</a>
+                            </dd>
+                            <dd class="t2">1月17日，央视鸡年春晚第二次正式联排在老台进行，运动员傅园剧演员聊天、自拍忙不停，一袭玫红色羽绒…</dd>
+                        </dl>
+                    </div>                   
+                </div>
+                <div class="right">
+                    <div class="ralatepic">
+                        <div class="ratit"><h2>明星写真推荐</h2></div>		
+                        <ul class="pics145" style="width:320px;">
+                            <li>
+                                <a href="http://www.myhouse.com/tuku/102/305767/" target="_blank" title="贾乃亮清新写真">
+                                    <i><img src="http://www.myhouse.com/upload/thumb/2017/01-10/0-VHFBjY.jpg"  alt="贾乃亮清新写真"/></i>
+                                    <h5>贾乃亮清新写真</h5>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="http://www.myhouse.com/tuku/102/305666/" target="_blank" title="王全安小女友左烨性感写真">
+                                    <i><img src="http://www.myhouse.com/upload/thumb/2016/11-25/0-5S1zjF.jpg"  alt="王全安小女友左烨性感写真"/></i>
+                                    <h5>王全安小女友左烨性感写真</h5>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="ralatepic">
+                        <div class="ratit"><h2>本周阅读排行榜</h2></div>		
+                        <ul class="newstop10">
+                            <li>
+                                <i class="i1">1</i>
+                                <a href="http://www.myhouse.com/news/102/304914001.html" target="_blank" title="谢贤终于脱掉那时髦的“墨镜”了">谢贤终于脱掉那时髦的“墨镜”了</a>
+                            </li>
+                            <li>
+                                <i class="i2">2</i>
+                                <a href="http://www.myhouse.com/news/102/305022001.html" target="_blank" title="郑嘉颖被警察带走 原来是因为拍电影违规">郑嘉颖被警察带走 原来是因为拍电影违规</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
             </div>
         </div>
-    </div>
-</div>
-<script>
-
-    var swiper = new Swiper('.swiper-container1', {
-        pagination: '.swiper-pagination',
-        autoplay : 3000,
-        paginationClickable: true,
-        loop : true
-    });
-    var swipers = new Swiper('.swiper-container', {
-
-        slidesPerView: 5,
-        direction: 'vertical',
-        paginationClickable: true,
-        spaceBetween: 9,
-        loop : true,
-        autoplay : 5000
-    });
-</script>
-</body>
+    </body>
 </html>
--- Public/Template/Home/Index/subject.php
+++ Public/Template/Home/Index/subject.php
@@ -1,8 +1,118 @@
-<?php
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <title>王耀庆_个人资料_图片_壁纸_电影_电视剧 - 明星网</title>
+        <link href="/Public/Static/Home/css/mytest.css" rel="stylesheet" type="text/css" />
+    </head>
+    <body class="mxk_ind">
+        <div class="sub_header_bg">
+            <div class="wrapper1220 sub_header">
+                <div class="sub_logo"><a href="http://www.myhouse.com/" alt="明星网"></a></div>
+                <div class="sub_nav"><a href="http://tuku.myhouse.com/mxxz/" target="_blank">写真</a><a href="http://tuku.myhouse.com/mxbz/" target="_blank">壁纸</a><a href="http://tuku.myhouse.com/ysjz/" target="_blank">剧照</a><a href="http://tuku.myhouse.com/hdxc/" target="_blank">现场</a><a href="http://news.myhouse.com/star/" target="_blank">八卦新闻</a><a href="http://www.myhouse.com/ziliao/" target="_blank">明星库</a></div>
+                <div class="search_box">
+                    <form id="form1" name="form1" method="get" target="_blank" action="http://so.myhouse.com/search.php">
+                        <input type="text" name="keyword" id="textfield" class="search_txt" value="请输入关键词" onfocus="javascript:this.value = ''" />
+                        <input type="submit" id="button" value="演员" class="search_btn" title="点击搜索" />
+                    </form>
+                </div>
+            </div>
+        </div>
+        <div class="mx_header_bg" style="background-color:#0fa6a4;">
+            <div class="wrapper1220 mx_header"> <h1 class="mx_name">王耀庆</h1>
+                <div class="txt"><h2><a href="http://www.myhouse.com/gangtai/wangyaoqing/" target="_blank" class="current">主页</a></h2><h2><a href="http://www.myhouse.com/gangtai/wangyaoqing/tupian.html" target="_blank">图片</a></h2><h2><a href="http://www.myhouse.com/gangtai/wangyaoqing/xinwen.html" target="_blank">星闻</a></h2><h2><a href="http://www.myhouse.com/gangtai/wangyaoqing/ziliao.html" target="_blank">个人资料</a></h2></div>
+            </div>
+        </div>
+        <div class="cm_block01_bg" >
+            <div class="wrapper1220 cm_block01" style=" background-color:#ebeae6;background-image:url(http://www.myhouse.com/css2015/skin/mx_img.jpg);"> <span class="img"><img src="http://img.myhouse.com/upload/thumb/2014/05-21/0-XIQLX4.jpg" alt="王耀庆"></span>
+                <h3 class="txt01">王耀庆（DavidWang），台湾艺人，籍贯山东，辅仁大学影像传播系学士，家中排行老大，已婚，育有2子。2006年之前在台湾发展，出演过多部电视剧，早些年拍过广告，广告金句奖“再忙，也要跟你喝杯咖啡”是王耀庆的广告代表作。2011年开始到内地发展，陆续出演了电影《失恋33天》，电视剧《浮沉》《小儿难养》等剧。2013年，在电视剧《小爸爸》中扮演泰勒人气飙升。2013年参演的电视剧还有《婚战》《大江东去》《产科医生》等。2013年获澳门国际电影电视节最佳男主角>></h3>
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+                <div class="txt02">出生：中国<br />星座：巨蟹座<br />身高：178<br />体重：58<br />职业：演员</div>
 
+                <div class="txt03"><b>分享：</b><div class="share_box"><div class="bdsharebuttonbox"><a href="#" class="bds_qzone" data-cmd="qzone" title="分享到QQ空间"></a><a href="#" class="bds_tsina" data-cmd="tsina" title="分享到新浪微博"></a></div>
+                    </div></div>
+            </div>
+        </div>
+        <div class="wrapper1186 cm_block02">
+            <div class="bottom_bg">               
+                <div class="block03">
+                    <div class="lm_name">
+                        <h2>王耀庆<span>.</span>最新消息</h2>
+                    </div>
+                    <dl>
+                        <dd>
+                            <a href="http://news.myhouse.com/read/93/277240001.html" target="_blank">
+                                <img src="http://img.myhouse.com/upload/thumb/925/277240.jpg" alt="《产科医生》：不谈恋爱谈正事儿"/>
+                            </a>
+                            <h3 class="txt01">
+                                <a href="http://news.myhouse.com/read/93/277240001.html" target="_blank">《产科医生》：不谈恋爱谈正事儿</a>
+                            </h3>
+                            <p class="txt02">近年来，国产电视剧中不乏以“医生”为主角，或以“医疗”为题材的作品，然而大多是“借壳上市”，表面打着…</p>
+                        </dd>
+                        <dd>
+                            <a href="http://news.myhouse.com/read/83/247245001.html" target="_blank">
+                                <img src="http://img.myhouse.com/upload/thumb/825/247245.jpg" alt="2012迅速蹿红荧幕十大新晋小生"/>
+                            </a>
+                            <h3 class="txt01">
+                                <a href="http://news.myhouse.com/read/83/247245001.html" target="_blank">2012迅速蹿红荧幕十大新晋小生</a>
+                            </h3>
+                            <p class="txt02">&nbsp; &nbsp; 看过《爱情公寓》这部爆笑系列喜剧的观众就一定知道那个阳光、帅真且充满喜感的大男孩孙艺洲…</p>
+                        </dd>
+                        <dd>
+                            <a href="http://news.myhouse.com/read/79/235400001.html" target="_blank">
+                                <img src="" alt="《林师傅》登陆卫视 王耀庆演艺事业高起点"/>
+                            </a>
+                            <h3 class="txt01">
+                                <a href="http://news.myhouse.com/read/79/235400001.html" target="_blank">《林师傅》登陆卫视 王耀庆演艺事业高起点</a>
+                            </h3>
+                            <p class="txt02">《林师傅在首尔》王耀庆剧照由林永健、张瑞希、宋丹丹、王耀庆等主演的电视《林师傅在首尔》已于2月26日登陆…</p>
+                        </dd>
+                    </dl>
+                    <ul id="xiaoxi">
+                        <li>
+                            <span>07-28</span><i>·</i>
+                            <a href="http://news.myhouse.com/read/93/277240001.html" target="_blank">《产科医生》：不谈恋爱谈正事儿</a>
+                        </li>
+                        <li>
+                            <span>08-14</span><i>·</i>
+                            <a href="http://news.myhouse.com/read/83/247245001.html" target="_blank">2012迅速蹿红荧幕十大新晋小生</a>
+                        </li>
+                        <li>
+                            <span>02-27</span><i>·</i>
+                            <a href="http://news.myhouse.com/read/79/235400001.html" target="_blank">《林师傅》登陆卫视 王耀庆演艺事业高起点</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="block04" id="gd_box02">
+                    <div class="lm_name">
+                        <h2>港台男明星人气榜</h2>
+                    </div>
+                    <div class="box">
+                        <div class="dl_box">
+                            <dl>
+                                <dd><i class="num01">01</i><a href="http://www.myhouse.com/gangtai/xietingfeng/" target="_blank"><img src="http://img.myhouse.com/upload/thumb/2014/12-31/0-6Hb9T0.jpg" title="谢霆锋" alt="谢霆锋"/></a>
+                                    <h3 class="txt01"><a href="http://www.myhouse.com/gangtai/xietingfeng/" target="_blank">谢霆锋</a></h3>
+                                    <span class="txt02">热度：20020</span><a href="http://www.myhouse.com/gangtai/xietingfeng/" target="_blank" class="btn">进入主页</a></dd><dd><i class="num02">02</i><a href="http://www.myhouse.com/gangtai/chenweiting/" target="_blank"><img src="http://img.myhouse.com/upload/thumb/2014/12-30/0-RpFjo4.jpg" title="陈伟霆" alt="陈伟霆"/></a>
+                                    <h3 class="txt01"><a href="http://www.myhouse.com/gangtai/chenweiting/" target="_blank">陈伟霆</a></h3>
+                                    <span class="txt02">热度：19802</span><a href="http://www.myhouse.com/gangtai/chenweiting/" target="_blank" class="btn">进入主页</a></dd><dd><i class="num03">03</i><a href="http://www.myhouse.com/gangtai/zhoujielun/" target="_blank"><img src="http://img.myhouse.com/upload/thumb/2014/12-30/0-6AnYhj.jpg" title="周杰伦" alt="周杰伦"/></a>
+                                    <h3 class="txt01"><a href="http://www.myhouse.com/gangtai/zhoujielun/" target="_blank">周杰伦</a></h3>
+                                    <span class="txt02">热度：18300</span><a href="http://www.myhouse.com/gangtai/zhoujielun/" target="_blank" class="btn">进入主页</a></dd><dd><i class="num04">04</i><a href="http://www.myhouse.com/gangtai/zhonghanliang/" target="_blank"><img src="http://img.myhouse.com/upload/thumb/2014/12-30/0-pJXvsl.jpg" title="钟汉良" alt="钟汉良"/></a>
+                                    <h3 class="txt01"><a href="http://www.myhouse.com/gangtai/zhonghanliang/" target="_blank">钟汉良</a></h3>
+                                    <span class="txt02">热度：16030</span><a href="http://www.myhouse.com/gangtai/zhonghanliang/" target="_blank" class="btn">进入主页</a></dd><dd><i class="num05">05</i><a href="http://www.myhouse.com/gangtai/linjunjie/" target="_blank"><img src="http://img.myhouse.com/upload/thumb/2014/12-30/0-CogSXa.jpg" title="林俊杰" alt="林俊杰"/></a>
+                                    <h3 class="txt01"><a href="http://www.myhouse.com/gangtai/linjunjie/" target="_blank">林俊杰</a></h3>
+                                    <span class="txt02">热度：15003</span><a href="http://www.myhouse.com/gangtai/linjunjie/" target="_blank" class="btn">进入主页</a></dd><dd><i class="num06">06</i><a href="http://www.myhouse.com/gangtai/chenguanxi/" target="_blank"><img src="http://img.myhouse.com/upload/thumb/2014/12-30/0-c3DLBP.jpg" title="陈冠希" alt="陈冠希"/></a>
+                                    <h3 class="txt01"><a href="http://www.myhouse.com/gangtai/chenguanxi/" target="_blank">陈冠希</a></h3>
+                                    <span class="txt02">热度：14020</span><a href="http://www.myhouse.com/gangtai/chenguanxi/" target="_blank" class="btn">进入主页</a></dd><dd><i class="num07">07</i><a href="http://www.myhouse.com/gangtai/liudehua/" target="_blank"><img src="http://img.myhouse.com/upload/thumb/2014/12-30/0-lDrmc7.jpg" title="刘德华" alt="刘德华"/></a>
+                                    <h3 class="txt01"><a href="http://www.myhouse.com/gangtai/liudehua/" target="_blank">刘德华</a></h3>
+                                    <span class="txt02">热度：12100</span><a href="http://www.myhouse.com/gangtai/liudehua/" target="_blank" class="btn">进入主页</a></dd><dd><i class="num08">08</i><a href="http://www.myhouse.com/gangtai/liukaiwei/" target="_blank"><img src="http://img.myhouse.com/upload/thumb/2014/12-31/0-O2S1KA.jpg" title="刘恺威" alt="刘恺威"/></a>
+                                    <h3 class="txt01"><a href="http://www.myhouse.com/gangtai/liukaiwei/" target="_blank">刘恺威</a></h3>
+                                    <span class="txt02">热度：12060</span><a href="http://www.myhouse.com/gangtai/liukaiwei/" target="_blank" class="btn">进入主页</a></dd>
+                            </dl>
+                        </div>
+                    </div>
+                </div>
+                <div style="clear:both"></div>
+            </div>
+        </div> 
+    </body>
+</html>
